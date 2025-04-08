1319cbea65fa4dac2dd40e1512566be2341e1a23
Frederic Stark <frederic.stark@gmail.com>
2025-04-08 16:21:18 +0200

    Small refactor

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,17 +1,14 @@
 #include "menu.hpp"
 
-#include "asm.hpp"
-
 void menu::add_item_path(std::shared_ptr<menu_action> item)
 {
     auto path = item->get_path();
     menu *current = this;
-    std::string name = "?";
 
     // all the menus
     while (path.size() != 1)
     {
-        name = path[0];
+        const std::string name = path[0];
         std::clog << name << "\n";
         auto submenu = get_submenu(name);
         if (!submenu)
@@ -62,4 +59,66 @@ void menu::emit(emiter &e)
     e.BYTE(0x00);  // End-of-menu
 
     std::clog << "<<<< Done" << std::endl;
+}
+
+std::vector<uint8_t> menu::menudef() const
+{
+    std::vector<uint8_t> result;
+    for (const auto &item : items_)
+    {
+        result.push_back(item->key());
+        auto pagedadrs = item->pagedadrs();
+        result.push_back(pagedadrs.get_page());
+        result.push_back(pagedadrs.get_address().get_low());
+        result.push_back(pagedadrs.get_address().get_high());
+        for (const auto &c : item->name()) result.push_back(c);
+        result.push_back(0);
+    }
+    result.push_back(0);
+    return result;
+}
+
+bool menu::is_item_present(const std::string &name) const
+{
+    for (const auto &item : items_)
+    {
+        if (item->name() == name)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::shared_ptr<menu> menu::get_submenu(const std::string &name) const
+{
+    for (const auto &menu : submenus_)
+    {
+        if (menu->name() == name)
+        {
+            return menu;
+        }
+    }
+    return nullptr;
+}
+
+void menu::dump()
+{
+    std::clog << "Menu [" << name() << "]" << std::endl;
+    std::clog << "Submenus:" << std::endl;
+    for (const auto &submenu : submenus_)
+    {
+        std::clog << "  " << submenu->name() << std::endl;
+    }
+    std::clog << "Items:" << std::endl;
+    for (const auto &item : items_)
+    {
+        std::clog << "  " << item->name() << std::endl;
+    }
+
+    std::clog << "Dump of Submenus" << std::endl;
+    for (const auto &submenu : submenus_)
+    {
+        submenu->dump();
+    }
 }
